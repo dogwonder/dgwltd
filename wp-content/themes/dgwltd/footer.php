12c0161f1e6c0945d48c035e9808026822ea22de
Rich Holman <dogwonder@gmail.com>
2021-01-15 10:42:57 +0000

    asset management

--- wp-content/themes/dgwltd/footer.php
+++ wp-content/themes/dgwltd/footer.php
@@ -50,9 +50,7 @@
 <script src="<?php echo get_template_directory_uri(); ?>/dist/scripts/govuk-frontend-3.5.0.min.js"></script>
 <script>window.GOVUKFrontend.initAll()</script>
 <script type="module" src="<?php echo get_template_directory_uri(); ?>/dist/scripts/app.js"></script>
-<?php /* ?>
 <?php include(locate_template('template-parts/_organisms/pswp.php')); ?>
 <script src="<?php echo get_template_directory_uri(); ?>/dist/scripts/gallery.js"></script>
-<?php */ ?>
 </body>
 </html>
