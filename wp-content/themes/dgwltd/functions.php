6b69bdd507acd9010bb9f1d16528f4819ef0df7c
Rich Holman <dogwonder@gmail.com>
2021-02-24 19:12:06 +0000

    Add CORS header

--- wp-content/themes/dgwltd/functions.php
+++ wp-content/themes/dgwltd/functions.php
@@ -132,8 +132,19 @@ if ( ! function_exists( 'dgwltd_setup' ) ) :
 		}
 		
 	}
+	
 endif;
 add_action( 'after_setup_theme', 'dgwltd_setup' );
+
+
+if ( ! function_exists( 'add_cors_http_header' ) ) :
+function add_cors_http_header() { 
+	header("Access-Control-Allow-Origin: *"); 
+	header("Access-Control-Allow-Methods: GET"); 
+	header("Access-Control-Allow-Headers: origin"); 
+}
+endif;
+add_action( 'after_setup_theme', 'add_cors_http_header' );
 
 
 //Remove admin stuff - e.g. Emojis
