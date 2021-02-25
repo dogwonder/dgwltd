48406c1b3f09ecd6bed28346484c5a1c3405177f
Rich Holman <dogwonder@gmail.com>
2021-02-25 11:40:40 +0000

    Add cors

--- wp-content/themes/dgwltd/functions.php
+++ wp-content/themes/dgwltd/functions.php
@@ -157,7 +157,7 @@ add_action('init', 'handle_preflight');
 function handle_preflight() {
     $origin = get_http_origin();
     if ($origin === 'https://dgw.ltd') {
-        header("Access-Control-Allow-Origin: array('dgw.ltd', 'pressablecdn.com')");
+        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
         header("Access-Control-Allow-Credentials: true");
         header('Access-Control-Allow-Headers: Origin, X-Requested-With, X-WP-Nonce, Content-Type, Accept, Authorization');
