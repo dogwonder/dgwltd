cf43675fc2ea1c1b0232afe362f33ee94eca4116
Rich Holman <dogwonder@gmail.com>
2021-02-26 14:45:35 +0000

    add css

--- wp-content/themes/dgwltd/header.php
+++ wp-content/themes/dgwltd/header.php
@@ -98,7 +98,7 @@ if(is_search() || is_404()) {
 					'container'	=> false
 				) );
 				?>
-				<button class="nav-toggle" id="nav-toggle" aria-label="Show or hide Top Level Navigation" aria-controls="nav-primary" aria-expanded="false">
+				<button style="display:none" class="nav-toggle" id="nav-toggle" aria-label="Show or hide Top Level Navigation" aria-controls="nav-primary" aria-expanded="false">
 					<svg class="open" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M436 124H12c-6.627 0-12-5.373-12-12V80c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12z"/></svg>
 					<svg class="close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><line x1="75" y1="75" x2="439" y2="439"/><line x1="439" y1="75" x2="75" y2="439"/></svg>
 					<span class="visually-hidden"><?php esc_html_e('Menu', 'dgwltd' ) ?></span>
